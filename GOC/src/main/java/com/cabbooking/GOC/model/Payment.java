40e3c430caf93727d1e1ddbe182b97c7698bdd65
Royal1542 <Bellapukonda.LokeshRoyal@cognizant.com>
2025-12-14 13:45:06 +0530

    Add files via upload

--- GOC/src/main/java/com/cabbooking/GOC/model/Payment.java
+++ GOC/src/main/java/com/cabbooking/GOC/model/Payment.java
@@ -1,4 +1,15 @@
 package com.cabbooking.GOC.model;
 
+import java.sql.Timestamp;
+
+import lombok.Data;
+
+@Data
 public class Payment {
+    private int paymentId;
+    private int bookingId;
+    private double amount;
+    private String paymentMethod;
+    private String paymentStatus;
+    private Timestamp paymentTime;
 }
