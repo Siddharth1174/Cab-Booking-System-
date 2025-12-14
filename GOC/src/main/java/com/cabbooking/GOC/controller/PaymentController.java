9e2cc419c7affb2728a4447dc8cddf07c5d4de77
Royal1542 <Bellapukonda.LokeshRoyal@cognizant.com>
2025-12-14 13:48:05 +0530

    Add files via upload

--- GOC/src/main/java/com/cabbooking/GOC/controller/PaymentController.java
+++ GOC/src/main/java/com/cabbooking/GOC/controller/PaymentController.java
@@ -1,4 +1,24 @@
 package com.cabbooking.GOC.controller;
+import com.cabbooking.GOC.dto.PaymentRequestDTO;
+import com.cabbooking.GOC.service.PaymentService;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/payment")
 public class PaymentController {
+    @Autowired
+    private PaymentService paymentService;
+    @PostMapping("/pay")
+    public String makePayment(@RequestBody PaymentRequestDTO  dto) {
+
+        paymentService.makePayment(dto);
+        return "Payment Sucessful";
+    }
+
 }
+
